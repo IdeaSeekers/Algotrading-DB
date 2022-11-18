4366de198c240dc3d47faa3181b4a34738ef3ab0
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-11-18 17:27:20 +0800

    Add: string hyperparameters

--- sql/init_tables.sql
+++ sql/init_tables.sql
@@ -23,6 +23,13 @@ CREATE TABLE IF NOT EXISTS IntHyperparameters (
     description TEXT
 );
 
+CREATE TABLE IF NOT EXISTS StringHyperparameters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    default_value Text,
+    description TEXT
+);
+
 CREATE TABLE IF NOT EXISTS IntHyperparametersOfStrategies (
     rel_id SERIAL PRIMARY KEY,
     strategy_id INT REFERENCES Strategies (id),
@@ -33,6 +40,12 @@ CREATE TABLE IF NOT EXISTS DoubleHyperparametersOfStrategies (
     rel_id SERIAL PRIMARY KEY,
     strategy_id INT REFERENCES Strategies (id),
     parameter_id INT REFERENCES DoubleHyperparameters (id)
+);
+
+CREATE TABLE IF NOT EXISTS StringHyperparametersOfStrategies (
+    rel_id SERIAL PRIMARY KEY,
+    strategy_id INT REFERENCES Strategies (id),
+    parameter_id INT REFERENCES StringHyperparameters (id)
 );
 
 CREATE TABLE IF NOT EXISTS Bots (
@@ -55,6 +68,14 @@ CREATE TABLE IF NOT EXISTS DoubleBotHyperparameters  (
     value DOUBLE PRECISION,
     bot_id INT REFERENCES Bots(id),
     parameter_id INT REFERENCES DoubleHyperparametersOfStrategies (rel_id),
+    UNIQUE (bot_id, parameter_id)
+);
+
+CREATE TABLE IF NOT EXISTS StringBotHyperparameters  (
+    rel_id SERIAL PRIMARY KEY,
+    value INT,
+    bot_id INT REFERENCES Bots(id),
+    parameter_id INT REFERENCES StringHyperparametersOfStrategies (rel_id),
     UNIQUE (bot_id, parameter_id)
 );
 
@@ -135,6 +156,18 @@ BEGIN
 END;
 $$;
 
+CREATE FUNCTION create_string_parameter(param_name VARCHAR, param_description TEXT, default_param_value TEXT)
+RETURNS INT
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    result INT;
+BEGIN
+   INSERT INTO StringHyperparameters(name, description, default_value) VALUES (param_name, param_description, default_param_value) RETURNING id INTO result;
+   RETURN result;
+END;
+$$;
 
 ------------------------------------------------------------------------------------------------------------------------------------------
 --
@@ -168,6 +201,19 @@ BEGIN
 END;
 $$;
 
+CREATE FUNCTION add_string_parameter(param_id INT, strat_id INT)
+RETURNS INT
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    ret INT;
+BEGIN
+   INSERT INTO StringHyperparametersOfStrategies(parameter_id, strategy_id) VALUES (param_id, strat_id) RETURNING rel_id INTO ret;
+   RETURN ret;
+END;
+$$;
+
 ------------------------------------------------------------------------------------------------------------------------------------------
 --
 --                                                        PARAMETERS SET
@@ -195,6 +241,20 @@ AS
 $$
 BEGIN
    INSERT INTO IntBotHyperparameters (value, bot_id, parameter_id) 
+    VALUES (new_value, cur_bot_id, rel_param_id)
+    ON CONFLICT (bot_id, parameter_id) DO UPDATE
+    SET value = new_value;
+    RETURN;
+END;
+$$;
+
+CREATE FUNCTION set_string_parameter(rel_param_id INT, cur_bot_id INT, new_value TEXT)
+RETURNS void
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+   INSERT INTO StringBotHyperparameters (value, bot_id, parameter_id) 
     VALUES (new_value, cur_bot_id, rel_param_id)
     ON CONFLICT (bot_id, parameter_id) DO UPDATE
     SET value = new_value;
@@ -274,6 +334,25 @@ BEGIN
 END;
 $$;
 
+CREATE FUNCTION get_string_parameter(param_id INT, cur_bot_id INT)
+RETURNS TEXT
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    default_val INT;
+BEGIN
+    default_val = (SELECT default_value 
+         FROM StringHyperparameters 
+         WHERE id = (
+            SELECT parameter_id 
+            FROM StringHyperparametersOfStrategies 
+            WHERE strategy_id = (SELECT get_bot_strategy(cur_bot_id))
+            )
+        );
+    RETURN (SELECT case count(*) when 0 then default_val else MAX(value) end FROM StringBotHyperparameters WHERE parameter_id = param_id AND bot_id = cur_bot_id);    
+END;
+$$;
 
 ------------------------------------------------------------------------------------------------------------------------------------------
 --
